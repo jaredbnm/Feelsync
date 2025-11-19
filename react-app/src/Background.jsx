e434e92c26d3f1a3b1518e57e262d61ceb875b0e
jaredbnm <jaredjr0204@gmail.com>
2025-11-19 11:44:19 -0600

    attempting gradient creation

--- react-app/src/Background.jsx
+++ react-app/src/Background.jsx
@@ -1,6 +1,9 @@
 function Background() {
+    const colorA = "#536976";
+    const colorB = "#292E49";
+    
     return (
-        <div className="min-h-screen bg-gradient-to-br from-blue-500 to-blue-300"></div>
+        <div className="min-h-screen bg-linear-to-br from-purple-500 via-blue-800 to-blue-300"></div>
     );
 }
 
