4a832dae6dc10adcd08da41fdee008568caa5eea
jaredbnm <jaredjr0204@gmail.com>
2025-11-22 16:21:16 -0600

    creating background gradient animation with blobs

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,8 @@
-import "./index.css";
+import Test from "./Test.jsx";
 
 function App() {
   return (
-    <div className="relative min-h-screen overflow-hidden bg-[#0e0e0e] flex items-center justify-center">
-      {/* Layered blobs */}
-      <div className="blob bg-[rgb(187_210_197)] w-[70vmax] h-[70vmax]" />
-      <div className="blob bg-[rgb(83_105_118)] w-[60vmax] h-[60vmax]" style={{ animationDelay: "-4s" }} />
-      <div className="blob bg-[rgb(41_46_73)] w-[65vmax] h-[65vmax]" style={{ animationDelay: "-8s" }} />
-    </div>
+    <Test />
   );
 }
 
---
+++ react-app/src/Test.jsx
@@ -0,0 +1,8 @@
+
+function Test() {
+    return (
+        <p className="text-white text-5xl">Feelsync</p>
+    );
+}
+
+export default Test;
