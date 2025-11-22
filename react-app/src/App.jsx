0f56c49fec6f07a48410593397cef95cf0f5282f
jaredbnm <jaredjr0204@gmail.com>
2025-11-22 15:22:41 -0600

    configuring tailwind issues

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,13 @@
-import Background from "./Background";
+import "./index.css";
 
 function App() {
   return (
-    <Background />
+    <div className="relative min-h-screen overflow-hidden bg-[#0e0e0e] flex items-center justify-center">
+      {/* Layered blobs */}
+      <div className="blob bg-[rgb(187_210_197)] w-[70vmax] h-[70vmax]" />
+      <div className="blob bg-[rgb(83_105_118)] w-[60vmax] h-[60vmax]" style={{ animationDelay: "-4s" }} />
+      <div className="blob bg-[rgb(41_46_73)] w-[65vmax] h-[65vmax]" style={{ animationDelay: "-8s" }} />
+    </div>
   );
 }
 
