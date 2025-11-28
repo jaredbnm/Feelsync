bccc4265de82925e47a06af4c7b539c5c4401af9
jaredbnm <jaredjr0204@gmail.com>
2025-11-28 12:14:08 -0500

    just testing font

--- react-app/src/Test.jsx
+++ react-app/src/Test.jsx
@@ -1,8 +1,12 @@
-
 function Test() {
-    return (
-        <p className="text-white text-5xl">Feelsync</p>
-    );
+  return (
+    <>
+      <p className="text-white text-5xl text-center">Feelsync</p>
+      <div className="text-white text-3xl text-center">
+        This is a test component to check if the Icona-Sans font is working.
+      </div>
+    </>
+  );
 }
 
 export default Test;
